3550cc33785358ad498d83f364439a657640dc70
Marius Rackwitz <git@mariusrackwitz.de>
2016-01-25 15:11:32 +0100

    Rebuild for improved deduplication

---
+++ install_subspecs/after/Pods/Target Support Files/PodTest-OSX/PodTest-OSX-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PodTest_OSX : NSObject
+@end
+@implementation PodsDummy_PodTest_OSX
+@end
---
+++ install_subspecs/after/Pods/Target Support Files/PodTest-iOS/PodTest-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PodTest_iOS : NSObject
+@end
+@implementation PodsDummy_PodTest_iOS
+@end
